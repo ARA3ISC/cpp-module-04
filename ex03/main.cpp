c85ff020e7b3204c4fac73663e34e3914d295c9e
ara3isc <m.aneddame01@gmail.com>
2023-10-03 20:02:36 +0100

    modified main ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 11:25:23 by maneddam          #+#    #+#             */
-/*   Updated: 2023/10/03 16:13:58 by maneddam         ###   ########.fr       */
+/*   Updated: 2023/10/03 20:02:20 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,65 +15,54 @@
 #include "Cure.hpp"
 #include "Ice.hpp"
 
-// int main()
-// {
-	// Character player("Kratos");
-	// AMateria* kabus = new Ice();
-	// AMateria* medkit = new Cure();
-
-	// player.equip(kabus);
-	// player.equip(medkit);
-
-
-	// Character pl2("player 2");
-
-
-	// pl2  = player;
-	// player.unequip(0);
-	// pl2.unequip(0);
-	// player.unequip(1);
-	// pl2.unequip(1);
-	// pl2 = player;
-
-	// std::cout << CYAN <<"new maeria " << pl2.slots[0]->getType() << RESET << std::endl;
-	// std::cout << CYAN <<"new naaame " << pl2._name << RESET << std::endl;
-
-	// player.equip(medkit); // full inventory if its the 5th materia
-
-	// player.use(0, player);
-	// player.use(4, player);
-
-	// player.unequip(0);
-	// player.unequip(1);
-
-	// delete kabus;
-	// delete medkit;
-// }
-
 int main()
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
+	ICharacter *player = new Character("Kratos");
+	AMateria* kabus = new Ice();
+	AMateria* medkit = new Cure();
 
+	player->equip(kabus);
+	player->equip(medkit);
 
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
+	player->unequip(0);
+	player->unequip(1);
 
-	me->equip(tmp);
+	player->equip(medkit); // full inventory if its the 5th materia
 
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	ICharacter* bob = new Character("bob");
-	me->use(0, *bob);
-	me->use(1, *bob);
+	player->use(0, *player);
+	player->use(1, *player);
 
-	// me->unequip(0);
-	// me->unequip(2);
+	player->unequip(0);
+	player->unequip(1);
 
-	delete bob;
-	delete me;
-	delete src;
+	delete kabus;
+	delete medkit;
 }
+
+// int main()
+// {
+// 	IMateriaSource* src = new MateriaSource();
+// 	src->learnMateria(new Ice());
+// 	src->learnMateria(new Cure());
+// 	ICharacter* me = new Character("me");
+
+
+// 	AMateria* tmp;
+// 	tmp = src->createMateria("ice");
+
+// 	me->equip(tmp);
+
+// 	tmp = src->createMateria("cure");
+// 	me->equip(tmp);
+// 	ICharacter* bob = new Character("bob");
+// 	me->use(0, *bob);
+// 	me->use(1, *bob);
+
+// 	// me->unequip(0);
+// 	// me->unequip(1);
+
+// 	delete bob;
+// 	delete me;
+// 	delete src;
+// }
 
