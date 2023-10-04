a843a28ba3311c43df84636c97b13b09dbef1d3c
ara3isc <m.aneddame01@gmail.com>
2023-10-04 16:05:35 +0100

    removig virtual keyword from Wrong..

--- ex00/WrongCat.hpp
+++ ex00/WrongCat.hpp
@@ -9,7 +9,7 @@ class WrongCat: public WrongAnimal
 		WrongCat();
 		WrongCat(const WrongCat& obj);
 		WrongCat& operator=(const WrongCat& obj);
-		virtual ~WrongCat();
+		~WrongCat();
 		void makeSound() const;
 };
 
